feae4683704bdc78ade259fcc303d87db4b6bd59
pravedens <107057525+pravedens@users.noreply.github.com>
2024-12-06 16:17:04 +0500

    Фильтры

---
+++ src/components/Group.vue
@@ -0,0 +1,105 @@
+<template>
+  <!-- Спикеры -->
+    <div class="relative">
+      <button
+        @click="isOpen = !isOpen"
+        type="button"
+        class="inline-flex items-center font-semibold text-gray-900 gap-x-1 text-sm/6"
+        aria-expanded="false"
+      >
+        <span>Год</span>
+        <svg
+          class="size-5"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true"
+          data-slot="icon"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </button>
+
+      <!--
+                Flyout menu, show/hide based on flyout menu state.
+            
+                Entering: "transition ease-out duration-200"
+                  From: "opacity-0 translate-y-1"
+                  To: "opacity-100 translate-y-0"
+                Leaving: "transition ease-in duration-150"
+                  From: "opacity-100 translate-y-0"
+                  To: "opacity-0 translate-y-1"
+              -->
+      <div
+        v-if="isOpen"
+        class="absolute z-10 max-w-xs px-4 mt-2 -translate-x-1/2 flexw-screen left-64"
+      >
+        <div
+          class="flex-auto w-screen max-w-xs overflow-hidden bg-white shadow-lg rounded-3xl text-sm/6 ring-1 ring-gray-900/5"
+        >
+          <div
+            :class="{ 'bg-slate-300': isActive == index }"
+            v-for="(item, index) in groups"
+            :key="index"
+            class="p-3 hover:bg-slate-300"
+          >
+            <a class="cursor-pointer" @click="filter(item.id, index)"
+              ><div class="flex items-center gap-x-6">
+                <div>
+                  <h3
+                    class="font-semibold tracking-tight text-gray-900 text-base/7"
+                  >
+                    {{ item.title }}
+                  </h3>
+                </div>
+                <svg
+                  v-if="isActive == index"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="text-green-600 size-5"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m4.5 12.75 6 6 9-13.5"
+                  />
+                </svg></div
+            ></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Спикеры -->
+</template>
+
+<script setup>
+import { onMounted, ref, defineEmits } from "vue";
+import axios from "axios";
+
+const emit = defineEmits(["filterPost"]);
+
+const isOpen = ref(false);
+
+const groups = ref([]);
+
+const isActive = ref("");
+
+onMounted(() => {
+  isActive.value = null;
+  axios
+    .get("groups")
+    .then((resp) => (groups.value = resp.data.data.data))
+    .catch((err) => console.log(err));
+});
+
+const filter = (id, index) => {
+  isActive.value = index;
+  emit("filterPost", id);
+};
+</script>
--- src/pages/PostView.vue
+++ src/pages/PostView.vue
@@ -1,34 +1,71 @@
 <template>
-    <div>
-        <Category @filterPost="isFilter" />
+  <div>
 
-        <ListPosts :dataPost="posts"/>
+    <div class="px-6 mx-auto max-w-7xl lg:px-8">
+    <p
+    class="mt-2 text-4xl font-semibold tracking-tight text-center text-gray-900 text-pretty sm:text-5xl lg:text-balance"
+  >
+    Проповеди
+  </p>
+    <div class="grid max-w-2xl grid-cols-2 pt-6 mx-auto mt-4 gap-x-4 gap-y-8 sm:mt-8 sm:pt-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+      <!--<div class="py-4 g-white sm:py-8">-->
+        <!--<div class="px-6 mx-auto max-w-7xl lg:px-8">-->
+          <!--<div class="max-w-2xl mx-auto lg:text-center">-->
 
-    </div>
+            <Category @filterPost="isFilter" />
+            <Conference @filterPost="conferenceFilter"/>
+            <Group @filterPost="groupFilter"/>
+          <!--</div>-->
+        <!--</div>-->
+      </div>
+    <!--</div>-->
+  </div>
+
+
+    <ListPosts :dataPost="posts" />
+  </div>
 </template>
 
 <script setup>
-import Category from '../components/Category.vue';
-import ListPosts from './ListPosts.vue';
+import Category from "../components/Category.vue";
+import ListPosts from "./ListPosts.vue";
+import Conference from "../components/Conference.vue";
+import Group from "../components/Group.vue";
 
 import { onMounted, ref } from "vue";
-  import axios from "axios";
-  
-  const posts = ref([]);
-  
-  onMounted( async () => {
-    await axios
-      .get("posts")
-      //.then(resp => console.log(resp))
-      .then((resp) => (posts.value = resp.data.data.data))
-      .catch((err) => console.log(err));
-  });
+import axios from "axios";
 
-  const isFilter = async (id) => {
-    await axios
-      .get("posts?category="+id)
-      //.then(resp => console.log(resp))
-      .then((resp) => (posts.value = resp.data.data.data))
-      .catch((err) => console.log(err));
-  }
+const posts = ref([]);
+
+onMounted(async () => {
+  await axios
+    .get("posts")
+    //.then(resp => console.log(resp))
+    .then((resp) => (posts.value = resp.data.data.data))
+    .catch((err) => console.log(err));
+});
+
+const isFilter = async (id) => {
+  await axios
+    .get("posts?category=" + id)
+    //.then(resp => console.log(resp))
+    .then((resp) => (posts.value = resp.data.data.data))
+    .catch((err) => console.log(err));
+};
+
+const conferenceFilter = async (id) => {
+  await axios
+    .get("posts?conference=" + id)
+    //.then(resp => console.log(resp))
+    .then((resp) => (posts.value = resp.data.data.data))
+    .catch((err) => console.log(err));
+};
+
+const groupFilter = async (id) => {
+  await axios
+    .get("posts?group=" + id)
+    //.then(resp => console.log(resp))
+    .then((resp) => (posts.value = resp.data.data.data))
+    .catch((err) => console.log(err));
+};
 </script>
